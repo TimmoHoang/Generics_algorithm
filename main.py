11b6f98e1594d728f02d15f6de531707ed6d4783
Timmy Neutron <tdhr3d@umsystem.edu>
2021-10-23 08:52:19 -0500

    Comiting from Home

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 # 
 
 import random
+import math
 
 weight = 0.00
 utility = 0.00
 count = 0
 one_gen = []
-
-
+total_fitness = 0
+#List to hold values of items.
+utility_weight_list = []
 while count < 1000:
-    initial_pop = []
-    for i in range(0, 400):
-        initial_pop.append(0)
+    initial_pop = [0] * 400
 
     while sum(initial_pop) != 20:
         item = random.randint(0, 399)
@@ -25,15 +25,39 @@ while count < 1000:
 with open("Program2Input.txt") as file:
 
     for data in file:
-        print(data)
-    # for i in range(0, 999):
-    #     sum = 0
-    #     for j in range(0, len(one_gen[i]) - 1):
-    #         if one_gen[i][j] == 1:
-    #         #Calculate fitness. utility and weight.
-    #         else:
-    #             continue
-    #    print(sum)
+        utility_weight_list.append(data.split())
+
+for i in range(0, 999): #one_gen indexing
+    fitness_score = 0
+    total_weight = 0
+    sum_utilities = 0
+    for j in range(0, len(one_gen[i]) - 1): #initial_pop indexing
+        if one_gen[i][j] == 1:
+            total_weight += float(utility_weight_list[j][1])
+            sum_utilities += float(utility_weight_list[j][0])
+
+        else:
+            continue
+    if total_weight > 500:
+        fitness_score = 1
+        total_fitness += fitness_score
+    else:
+        fitness_score = sum_utilities
+        total_fitness += fitness_score
+
+#L2 Distribution
+for i in range(0, 999):
+    L2_fitness = 0
+    for j in range(0, len(one_gen[i]) - 1): #initial_pop indexing
+        if one_gen[i][j] == 1:
+            total_weight += float(utility_weight_list[j][1])
+            sum_utilities += float(utility_weight_list[j][0])
+            if total_weight > 500:
+                fitness_score = 1
+
+            else:
+                fitness_score = sum_utilities
+
 
 
 
