f9f671646eacbb2a028b95f3b9b2a5080f9bcc95
Timmy Neutron <tdhr3d@umsystem.edu>
2021-10-24 23:46:33 -0500

    Assignment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# 
+# Timothy Hoang CS 461- Program 2
 
 import random
 import math
@@ -49,48 +49,107 @@ def CDF_Func(lst):
         lst[i] += lst[i - 1]
     return lst
 
-def New_Parent(lst):
+def New_Parent(lst, old_parent):
     new_parent = []
     random_float = random.random()
     for i, value in enumerate(lst):
 
         if value > random_float:
-            new_parent.extend(one_gen[i:])
-    return new_parent
+            new_parent.extend(old_parent)
+            return new_parent
+
+def Mutate_Gene(child):
+    for i in range(0, len(child)):
+        mutation = random.randint(1, 10000)
+        if mutation == 1:
+            if child[i] == 1:
+                child[i] = 0
+            else:
+                child[i] = 1
+    return child
 
 ## Read Data from file.
 utility_weight_list = []
-
+gen_size = 1000
+pop_size = 400
+start_pop = 500
+generations = 5000
+max_fitness_list = []
+average_fitness_list = []
+count = 0
 with open("Program2Input.txt") as file:
 
     for data in file:
         utility_weight_list.append(data.split()) #list holding all data
 
-## generate generation and population using sizes.
-one_gen = Generate_Pop(1000, 400)  ##first generation
+one_gen = Generate_Pop(gen_size, pop_size)  ##first generation
 
-# total_fitness, sum_of_fitness = Fitness_Func(one_gen, utility_weight_list) #total_fitness ** 2
+## Iterate on generations count
+while count < generations:
+    average_fitness = 0
+    max_fitness = 0
+    child_list = []
+    count_1 = 0
+    new_parents = []
 
-fitness_list = Fitness_Func(one_gen, utility_weight_list)  # list of all fitness score
+    fitness_list = Fitness_Func(one_gen, utility_weight_list)  # list of all fitness score
 
-squared_fitness = [pow(fitness, 2) for fitness in fitness_list]
 
-L2_total_fitness = 0
+    squared_fitness = [pow(fitness, 2) for fitness in fitness_list]
 
-L2_list = L2_Func(squared_fitness) ## lsit of all L2 probabilities
 
-CDF_values = CDF_Func(L2_list)
-new_p = New_Parent(CDF_values)
+    L2_list = L2_Func(squared_fitness) ## lsit of all L2 probabilities
 
-random_child_value = random.randint(0, 400)
+    CDF_values = CDF_Func(L2_list)
+    new_parents = New_Parent(CDF_values, one_gen)
 
-new_child_1 = (one_gen[random_child_value:] + new_p[:random_child_value])
+    while count_1 < start_pop:
 
-new_child_2 = (one_gen[:random_child_value] + new_p[random_child_value:])
+        ## trying to splice up DNA and make children
+        random_gen_index = random.randint(0, len(new_parents) - 1)
+        random_child_value = random.randint(0, pop_size - 1)
 
-### Splice DNA and create 2 children (crossover)
+        new_child_1 = (one_gen[random_gen_index][random_child_value:] + new_parents[random_gen_index][:random_child_value])
+
+
+        new_child_2 = (one_gen[random_gen_index][:random_child_value] + new_parents[random_gen_index][random_child_value:])
+
+        ## mutate 1/10000.
+        new_child_1 = Mutate_Gene(new_child_1)
+        new_child_2 = Mutate_Gene(new_child_2)
+
+        child_list.append(new_child_1)
+        child_list.append(new_child_2)
+        count_1 += 1
+
+
+    one_gen = child_list #set new children list as the generation
+
+    ## try to keep trach of max and average fitness
+    average_fitness = sum(fitness_list) / len(fitness_list)
+    average_fitness_list.append(average_fitness)
+    max_fitness = max(fitness_list)
+    max_fitness_list.append(max_fitness)
+
+    #test each iteration
+    print("Max fitness gen ", count, " : ", max_fitness)
+    print("Average fitness gen ", count, " :", average_fitness)
+
+    f = open("outputfile.txt", "w")
+    L = ["Generation ", str(generations), "\nMax per generation: ", str(max_fitness), "\nAverage per generation: ", str(average_fitness)]
+    f.writelines(L)
+    f.close()
+    count += 1
+
+print("Starting population: ", start_pop)
+print("Max fitness after ", generations, " generations: ", max_fitness_list[len(max_fitness_list) - 1])
+print("Average fitness: ", average_fitness_list[len(average_fitness_list) - 1])
+
+
 
 ## Roll for Mutation 1/10000(0.00001) Success roll = flip random -> repeat one generation -> repeat program.
+
+## record average fitness and max fitness
 
 
 
